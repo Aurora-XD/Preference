39a703f91d6cf7b74a9796dc35a09207d2310b78
yangchen <chen.yang1@thoughtworks.com>
2020-07-24 22:08:44 +0800

    update editTex by button update

--- app/src/main/java/com/example/preference/SPActivity.java
+++ app/src/main/java/com/example/preference/SPActivity.java
@@ -9,9 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SPActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
+
+    private SharedPreferences.Editor editor;
 
     @BindView(R.id.sp_edit)
     EditText mEditText;
@@ -22,10 +25,18 @@ public class SPActivity extends AppCompatActivity {
         setContentView(R.layout.activity_s_p);
 
         sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
         ButterKnife.bind(this);
 
         setEditView();
+    }
+
+    @OnClick(R.id.sp_update)
+    void updateEditText() {
+        String value = mEditText.getText().toString();
+        editor.putString("name", value);
+        editor.commit();
     }
 
     private void setEditView() {
